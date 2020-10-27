90c0c1a9b6011997b364ef949b1a8ab8dd2af7ea
Jiwhan Son <jiwhanson@utexas.edu>
2020-10-27 13:30:48 -0500

    Adding builder pattern to petType, make petController tests

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -15,7 +15,7 @@ import java.util.Set;
 public class Owner extends Person {
 
     @Builder
-        public Owner(Long id, String firstName, String lastName, String address, String city,
+    public Owner(Long id, String firstName, String lastName, String address, String city,
                  String telephone, Set<Pet> pets) {
         super(id, firstName, lastName);
         this.address = address;
